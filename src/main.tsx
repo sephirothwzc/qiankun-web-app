8e114f20e839202b0959f9f320fbc40bc7edacfd
zhanchao.wu <zhanchao.wu@icloud.com>
2022-03-29 15:56:15 +0800

    feat(qiankun): qiankun

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+/**
+ * 加载微应用
+ * @param props
+ */
+function render(props: any) {
+  const { container } = props;
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    container ? container.querySelector('#root') : document.getElementById('root')
+  );
+}
+
+renderWithQiankun({
+  mount(props) {
+    console.log('mount');
+    render(props as any);
+  },
+  bootstrap() {
+    console.log('bootstrap');
+  },
+  unmount(props: any) {
+    console.log('unmount');
+    const { container } = props;
+    const mountRoot = container?.querySelector('#root');
+    ReactDOM.unmountComponentAtNode(mountRoot || document.querySelector('#root'));
+  },
+});
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  console.log('我正在作为独立应用运行');
+  render(document.getElementById('root') as any);
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { join } from 'path';
+import qiankun from 'vite-plugin-qiankun';
+
+/**
+ * useDevMode 开启时与热更新插件冲突,使用变量切换
+ * 因为开发环境作为子应用时与热更新插件（可能与其他修改html的插件也会存在冲突）有冲突，
+ * 所以需要额外的调试配置
+ * useDevMode = true 则不使用热更新插件，useDevMode = false 则能使用热更新，但无法作为子应用加载。
+ */
+const useDevMode = true;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), qiankun('digital-marketing', { useDevMode })],
+  // 生产环境需要指定运行域名作为base
+  base: 'http://localhost:8098',
   css: {
     modules: {
       // css模块化 文件以.module.[css|less|scss]结尾
@@ -27,7 +38,7 @@ export default defineConfig({
     ],
   },
   server: {
-    port: 8099,
+    port: 8098,
     open: true,
     proxy: {
       // 字符串简写方式
